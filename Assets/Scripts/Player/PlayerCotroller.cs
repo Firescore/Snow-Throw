0fda7e8890e92bf80f35d6e031d09b7e1cdf7846
dhiraj7894 <dhiraj7894@gmail.com>
2020-10-09 23:20:17 +0530

    URP

--- Assets/Scripts/Player/PlayerCotroller.cs
+++ Assets/Scripts/Player/PlayerCotroller.cs
@@ -5,19 +5,34 @@ using UnityEngine;
 public class PlayerCotroller : MonoBehaviour
 {
     public GameObject Player;
+    public CameraFollow cF;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("R-15"))
         {
             Player.transform.rotation = Quaternion.Euler(-15, 180, 0);
         }
-        if (other.gameObject.CompareTag("R+15"))
+        if (other.gameObject.CompareTag("R-0"))
         {
             Player.transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+        if (other.gameObject.CompareTag("R-0-2"))
+        {
+            Player.transform.rotation = Quaternion.Euler(0, 90, 0);
+            Player.transform.position = new Vector3(Player.transform.position.x, -6.5f, Player.transform.position.z);
+            cF.Offset = new Vector3(-16f, 0f, -8f);
+            cF.Offset = new Vector3(-16f, 0f, -8f);
+        }
+        if (other.gameObject.CompareTag("R+15"))
+        {
+            Player.transform.rotation = Quaternion.Euler(15, 90, 0);
+            cF.Offset = new Vector3(-16f, 7f, -8f);
         }
         if (other.gameObject.CompareTag("R+90"))
         {
             Player.transform.rotation = Quaternion.Euler(0, 90, 0);
+            Player.transform.position = new Vector3(Player.transform.position.x, 5.98f, Player.transform.position.z);
+            cF.Offset = new Vector3(-16f, 0f, -8f);
         }
     }
 }
